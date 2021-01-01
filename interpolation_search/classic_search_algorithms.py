fd3d6da84b02b0ebf3e061103411882d220fff94
Matt Woolley <matt.wool@live.com.au>
2021-01-01 23:38:55 +1100

    Implement first occurance binary search

--- interpolation_search/classic_search_algorithms.py
+++ interpolation_search/classic_search_algorithms.py
@@ -2,12 +2,14 @@ def sequential_search(array: list, element) -> int:
     for idx, array_el in enumerate(array):
         if array_el == element:
             return idx
-    
+
     raise ValueError(f"'{element}' is not in list")
+
 
 def binary_search(array: list, element) -> int:
 
     return binary_search_recursive(array, element, 0, len(array))
+
 
 def binary_search_recursive(array: list, element, start: int, end: int) -> int:
     """Code ripped from https://stackabuse.com/binary-search-in-python/"""
@@ -16,11 +18,10 @@ def binary_search_recursive(array: list, element, start: int, end: int) -> int:
         raise ValueError(f"'{element}' is not in list")
 
     mid = (start + end) // 2
-    mid_value = array[mid]
-    if element == mid_value:
+    if element == (mid_value := array[mid]):
         return mid
 
     if element < mid_value:
-        return binary_search_recursive(array, element, start, mid-1)
+        return binary_search_recursive(array, element, start, mid - 1)
     else:
-        return binary_search_recursive(array, element, mid+1, end)
+        return binary_search_recursive(array, element, mid + 1, end)
