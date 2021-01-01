a9340744e5f029dbcba45365287d80aeed79e9a5
Matt Woolley <matt.wool@live.com.au>
2021-01-01 17:57:21 +1100

    Fix interpolation probe formula

--- distribution_search/interpolation_search.py
+++ distribution_search/interpolation_search.py
@@ -1,19 +1,24 @@
 def _interpolate_index(element, min_idx, max_idx, min_value, max_value):
 
-    nelements = max_idx - min_idx + 1
-    normalised_interpolated_value = (element - min_value) / (max_value - min_value)
-    return int(normalised_interpolated_value * nelements) + min_idx
+    if (value_range := max_value - min_value) == 0:
+        return min_idx
+
+    normalised_interpolated_value = (element - min_value) / value_range
+    delta_idx = max_idx - min_idx
+    return int(normalised_interpolated_value * delta_idx) + min_idx
 
 
 def _interpolation_search_recursive(array: list, element, min_idx: int, max_idx: int) -> int:
 
-    if min_idx > max_idx:
+    if min_idx > max_idx or element < (min_value := array[min_idx]) or element > (max_value := array[max_idx]):
         raise ValueError(f"'{element}' is not in list")
 
     interpol_idx = _interpolate_index(element, min_idx, max_idx, array[min_idx], array[max_idx])
     interpol_val = array[interpol_idx]
 
     if element == interpol_val:
+        while (temp_interpol_idx := interpol_idx - 1) >= 0 and array[temp_interpol_idx] == element:
+            interpol_idx = temp_interpol_idx
         return interpol_idx
 
     if element < interpol_val:
--- tests/test_interpolation_search.py
+++ tests/test_interpolation_search.py
@@ -38,9 +38,19 @@ class TestInterpolationSearch(unittest.TestCase):
         self.assertEqual(interpolation_search(array, element), 1)
 
     def test_get_index_of_first_occurance_all_elements_same(self):
-        array = [1, 1, 1, 1]
+        array = [1] * 4
         element = 1
         self.assertEqual(interpolation_search(array, element), 0)
+
+    def test_get_index_of_first_occurance_all_elements_zero(self):
+        array = [0] * 4
+        element = 0
+        self.assertEqual(interpolation_search(array, element), 0)
+
+    def test_get_index_in_large_list(self):
+        array = list(range(0, 100000000))   # 100 million
+        element = 50512546
+        self.assertEqual(interpolation_search(array, element), element)
     
     # Raises error
     def test_empty_list_raises_error(self):
@@ -58,7 +68,7 @@ class TestInterpolationSearch(unittest.TestCase):
         element = 5
         self.assertRaises(ValueError, interpolation_search, array, element)
 
-    def test_element_not_in_array_between_elements_raises_error(self):
+    def test_element_between_elements_raises_error(self):
         array = [1, 2, 3, 4]
         element = 3.5
         self.assertRaises(ValueError, interpolation_search, array, element)
