cfb7a60a39d0aa4c96e2a45bf68c1c88f703c724
WoolleySheep <37137527+WoolleySheep@users.noreply.github.com>
2021-01-01 23:40:21 +1100

    Implement binary search for first occurance (#5)

--- interpolation_search/interpolation_search.py
+++ interpolation_search/interpolation_search.py
@@ -2,6 +2,19 @@
 # Email: matt.wool@live.com.au
 
 
+def _first_occurance_binary_method(array, min_idx, match_idx):
+
+    if (delta_idx := match_idx - min_idx) <= 0:
+        return match_idx
+
+    halfway_idx = delta_idx // 2 + min_idx
+
+    if array[halfway_idx] == array[match_idx]:
+        return _first_occurance_binary_method(array, min_idx, halfway_idx)
+    else:
+        return _first_occurance_binary_method(array, halfway_idx + 1, match_idx)
+
+
 def _interpolate_index(element, min_idx, max_idx, min_value, max_value):
 
     if (value_range := max_value - min_value) == 0:
@@ -27,12 +40,8 @@ def _interpolation_search_recursive(
     interpol_val = array[interpol_idx]
 
     if element == interpol_val:
-        while (temp_interpol_idx := interpol_idx - 1) >= 0 and array[
-            temp_interpol_idx
-        ] == element:
-            interpol_idx = temp_interpol_idx
-        return interpol_idx
-
+        return _first_occurance_binary_method(array, min_idx, interpol_idx)
+    
     if element < interpol_val:
         return _interpolation_search_recursive(
             array, element, min_idx, interpol_idx - 1
@@ -46,3 +55,8 @@ def _interpolation_search_recursive(
 def interpolation_search(array: list, element) -> int:
 
     return _interpolation_search_recursive(array, element, 0, len(array) - 1)
+
+
+array=[-97, -96, -94, -91, -90, -88, -87, -85, -85, -84, -80, -77, -77, -76, -73, -67, -66, -66, -60, -58, -58, -51, -41, -38, -37, -37, -36, -33, -32, -32, -31, -30, -25, -21, -20, -19, -19, -17, -16, -12, -10, -8, 2, 2, 6, 7, 14, 14, 19, 21, 23, 23, 26, 28, 31, 35, 40, 40, 41, 48, 60, 60, 65, 67, 70, 70, 75, 78, 82, 84, 87, 88, 90, 93, 93, 93, 93, 95, 95, 96, 97, 97, 98]
+element=93
+interpolation_search(array, element)
