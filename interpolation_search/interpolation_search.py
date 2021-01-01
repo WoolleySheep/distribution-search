d925618ffc4508daf8537ad253577dd8d15cf402
Matt Woolley <matt.wool@live.com.au>
2021-01-02 00:21:04 +1100

    Implement jump search function

--- interpolation_search/interpolation_search.py
+++ interpolation_search/interpolation_search.py
@@ -41,7 +41,7 @@ def _interpolation_search_recursive(
 
     if element == interpol_val:
         return _first_occurance_binary_method(array, min_idx, interpol_idx)
-    
+
     if element < interpol_val:
         return _interpolation_search_recursive(
             array, element, min_idx, interpol_idx - 1
@@ -55,8 +55,3 @@ def _interpolation_search_recursive(
 def interpolation_search(array: list, element) -> int:
 
     return _interpolation_search_recursive(array, element, 0, len(array) - 1)
-
-
-array=[-97, -96, -94, -91, -90, -88, -87, -85, -85, -84, -80, -77, -77, -76, -73, -67, -66, -66, -60, -58, -58, -51, -41, -38, -37, -37, -36, -33, -32, -32, -31, -30, -25, -21, -20, -19, -19, -17, -16, -12, -10, -8, 2, 2, 6, 7, 14, 14, 19, 21, 23, 23, 26, 28, 31, 35, 40, 40, 41, 48, 60, 60, 65, 67, 70, 70, 75, 78, 82, 84, 87, 88, 90, 93, 93, 93, 93, 95, 95, 96, 97, 97, 98]
-element=93
-interpolation_search(array, element)
